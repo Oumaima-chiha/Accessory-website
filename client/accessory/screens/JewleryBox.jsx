ee80a1299e5468a0bb7454fdc0a5321995d5b5a9
Oumaima-chiha <chihaouma@gmail.com>
2024-01-03 02:09:14 +0100

    search/filter by category done

--- client/accessory/screens/JewleryBox.jsx
+++ client/accessory/screens/JewleryBox.jsx
@@ -17,15 +17,18 @@ const JewelryItem = ({ item, onPress }) => {
   );
 };
 
-const JewelryBox = ({searchTerm}) => {
+const JewelryBox = ({searchTerm,selectedCategory}) => {
   const [jewelryItems, setJewelryItems] = useState([]);
   const navigation = useNavigation();
-  const [filterData, setFilterData] = useState([])
+  const [filterData, setFilterData] = useState([]);
+  
+
+ 
 
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`http://${process.env.EXPO_PUBLIC_API_URL}:3000/api/Jewelry`);
+      const response = await axios.get(`http://192.168.1.3:3000/api/Jewelry`);
       if (response.status === 200) {
         setJewelryItems(response.data);
         setFilterData(response.data);
@@ -54,7 +57,27 @@ const JewelryBox = ({searchTerm}) => {
       console.log(jewelryItems)
     }
   }, [searchTerm]);
+  useEffect(() => {
+    if (selectedCategory === "All") {
+      setFilterData(jewelryItems);
+    } else {
+      const filteredList = jewelryItems.filter((elem) => {
+        if (Array.isArray(elem.category)) {
+          const foundCategory = elem.category.find(
+            (category) =>
+              category &&
+              typeof category === "string" &&
+              category.toLowerCase() === selectedCategory.toLowerCase()
+          );
+          return foundCategory !== undefined;
+        }
+        return false;
+      });
+      setFilterData(filteredList);
+    }
+  }, [selectedCategory]);
 
+ 
 
   const handleItemPress = (item) => {
     navigation.navigate('details', { item });
