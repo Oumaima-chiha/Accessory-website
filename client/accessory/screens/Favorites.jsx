8479caf62a61e48168d0b0ae93fd39711220fd91
ghssen.mellassi <ghassen.mellasi@proxym-it.com>
2024-01-22 22:05:41 +0100

    feat: 🎸 Favorites feature functions

--- client/accessory/screens/Favorites.jsx
+++ client/accessory/screens/Favorites.jsx
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import axios from "../services/axiosInterceptor";
 
 const Favorites = () => {
+  const [favoriteList,setFavoriteList]=useState();
+  const fetchData=async ()=>{
+    try {
+      const resp=await axios.get('jewelry/favorites')
+      if (resp.status === 200) {
+        setFavoriteList(resp.data);
+        console.log(resp.data);
+      }
+    }
+    catch (err)
+    {
+     console.log(err)
+    }
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+  const removeFromFavorites= async (id) => {
+    try {
+      const res = await axios.delete(
+          "jewelry/favorite/" + id
+      );
+      if (res.status === 204)
+        setFavoriteList((prev) => {
+          const filteredItems = prev?.items?.filter((item) => item.id !== id);
+          return { ...prev, items: filteredItems };
+        });
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 401) {
+      }
+      console.log(error.message);
+    }
+  };
+  const removeAllFavorites= async () => {
+    try {
+      const res = await axios.delete(
+          "jewelry/favorites"
+      );
+      if (res.status === 204)
+        setFavoriteList((prev) => {
+          return { ...prev, items: [],totalFavorites:0 };
+        });
+    } catch (error) {
+      console.log(error);
+      if (error.response.status === 401) {
+      }
+      console.log(error.message);
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.contentContainer}>
         <MaterialCommunityIcons name="heart" size={40} color="#FF6F61" />
         <Text style={styles.title}>Your Favorites</Text>
         <Text style={styles.subtitle}>Discover and save your favorite items</Text>
-        {/* Add more components or styling as needed */}
       </View>
     </SafeAreaView>
   );
